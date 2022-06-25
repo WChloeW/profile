a37d0ce682427a22d229fe85665c7192e4c6fdf2
weiwei <chenxinwei229@foxmail.com>
2022-06-25 19:58:55 +0800

    feat: paperList demo

--- src/components/list/index.jsx
+++ src/components/list/index.jsx
@@ -1,11 +1,25 @@
 import { Divider, List, Typography } from "antd";
-import './index.css';
-const data = [
-  "Racing car sprays burning fuel into crowd.",
-  "Japanese princess to wed commoner.",
-  "Australian walks 100km after outback crash.",
-  "Man charged over missing wedding girl.",
-  "Los Angeles battles huge wildfires.",
+import "./index.css";
+
+const dataDemo = [
+  {
+    paperName:
+      "Association of Baseline Patient-reported Health-related Quality of Life Metrics with Outcome in Localised Prostate Cancer",
+    author: "S Roy,S C Morgan",
+    info: "CLINICAL ONCOLOGY, no. 1 (2022): E61-E68",
+  },
+  {
+    paperName:
+      "Association of Baseline Patient-reported Health-related Quality of Life Metrics with Outcome in Localised Prostate Cancer",
+    author: "S Roy,S C Morgan",
+    info: "CLINICAL ONCOLOGY, no. 1 (2022): E61-E68",
+  },
+  {
+    paperName:
+      "Association of Baseline Patient-reported Health-related Quality of Life Metrics with Outcome in Localised Prostate Cancer",
+    author: "S Roy,S C Morgan",
+    info: "CLINICAL ONCOLOGY, no. 1 (2022): E61-E68",
+  },
 ];
 
 const ListPaper = () => (
@@ -16,8 +30,22 @@ const ListPaper = () => (
         header={<div>Paper</div>}
         footer={<div>Footer</div>}
         bordered
-        dataSource={data}
-        renderItem={(item) => <List.Item>{item}</List.Item>}
+        dataSource={dataDemo}
+        renderItem={(item) => (
+          <div>
+            <List.Item>
+              <List.Item.Meta
+                title={<a href="">{item.paperName}</a>}
+                description={
+                  <>
+                    <div>{item.author}</div>
+                    <div>{item.info}</div>
+                  </>
+                }
+              />
+            </List.Item>
+          </div>
+        )}
       />
     </div>
   </>
