6b5e6f282ee16c4fdb65e1f234993bd051d6e839
weiwei <chenxinwei229@foxmail.com>
2022-06-25 20:32:03 +0800

    feat: add data

--- src/components/relationship/ForceLayoutChart/datatest.js
+++ src/components/relationship/ForceLayoutChart/datatest.js
@@ -3,7 +3,7 @@
 let testdata = {
     nodes: [
       {
-        name: `a`,
+        name: `陈列平`,
         category: 0,
         value: 50,
         x: 0,
@@ -29,30 +29,38 @@ let testdata = {
     ],
   };
   //随机生成数据
-  for (let i = 1; i < 30; i++) {
-    const nameArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','aa','bb','cc','vv','ee','ww','ii','yy','pp','wef','sd','rgy','weowe','sdf','h5h','xcox'];
+
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //不含最大值，含最小值
+  }
+
+  for (let i = 1; i < 21; i++) {
+    const nameArray= ['陈列平','K,Tamada', 'Sheng,Yao', 'Xiaojie,Wang', 'Garth L,Warnock', 'Haiying,Xu', 'Dawei,Ou', 'Koji,Tamada', 'Jianqiang,Hao', 'Daniel L,Metzger', 'Ziliang,Ao', 'Gefeng,Zhu', 'Miguel F,Sanmamed', 'Ignacio,Melero', 'David L,Rimm', '王盛典', 'Houot, Roch', 'Alice L.-F,Mui', 'H,Tamura', 'Luo, Liqun', 'Levy, Ronald'];
     console.log(nameArray.length);
 
     testdata?.nodes?.push({
       name: nameArray[i],
-      category: i < 20 ? 1 : 2,
+      category: i < 15 ? 1 : 2,
       value: i,
       x: (Math.random() < 0.5 ? -1 : 1) * Math.random() * 90,
       y: (Math.random() < 0.5 ? -1 : 1) * Math.random() * 90,
       // symbolSize: i * 2,
       // fixed: true,
     });
-    if (i < 20)
+    if (i < 15)
       testdata?.links.push({
         source: nameArray[0],
         target: nameArray[i],
       });
     else
       testdata?.links.push({
-        source: nameArray[parseInt(Math.round(Math.random()*10+10))],
+        source: nameArray[getRandomInt(5,15)],
         target: nameArray[i],
       });
   }
   console.log(testdata);
+  console.log(parseInt(Math.random() * 10) + 2);
   export {testdata};
   
